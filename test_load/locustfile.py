dbfdfb598bea5c7ca25cdbba48ce18e9f89754d6
Mufid Jamaluddin <mufid.jamaluddin@sinbad.co.id>
2023-12-29 04:47:35 +0700

    fix: Load Testing Code

--- test_load/locustfile.py
+++ test_load/locustfile.py
@@ -18,7 +18,7 @@ class MyUser(HttpUser):
     wait_time = between(1, 3)
 
     @task
-    def v2_add_cart_sr(self):
+    def answer_question(self):
         headers = {
             'Content-Type': 'application/json',
         }
