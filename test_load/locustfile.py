cea8e4a70e0100858233a2e2882d98997dc22ab4
Mufid Jamaluddin <mufid.jamaluddin@sinbad.co.id>
2024-02-19 08:24:48 +0700

    feat: Add Test Data

---
+++ test_load/locustfile.py
@@ -0,0 +1,32 @@
+from locust import HttpUser, task, between
+from random import randrange
+
+test_data = [
+    'Kapan Diklat diadakan oleh OSIS Seksi Bidang 4, dan siapa yang bisa mengikuti sebagai peserta?',
+    'Berapa frekuensi pelaksanaan "Pembiasaan Senam Pagi" setiap bulannya?',
+    'Apa yang menjadi fokus kegiatan "Bulan Bahasa" di Kampus SMAN SITURAJA untuk Seksi Bidang 8?',
+    'Mengapa pembinaan kesiswaan perlu mendukung kreativitas, keterampilan, dan kewirausahaan?',
+    'Apa tujuan dari program Memperingati Maulid Nabi Muhammad SAW?',
+    'Mengapa kondisi yang tertib, keamanan terjaga, dan situasi kondusif dianggap esensial dalam pendidikan?',
+    'Bagaimana Wakil Kepala Bidang Humas berkoordinasi dengan instansi pemerintah dan dinas terkait?',
+    'Apa kegiatan tahunan yang diselenggarakan oleh Wakil Kepala Bidang Humas untuk seluruh warga SMAN Situraja?',
+    'Bagaimana sekolah mengukur aspek Kreatif dalam mencapai visinya?',
+    'Siapakah yang merupakan Kepala Sekolah pertama di SMA Negeri Situraja dan kapan beliau menjabat?',
+]
+
+class MyUser(HttpUser):
+    wait_time = between(1, 3)
+
+    @task
+    def answer_question(self):
+        headers = {
+            'Content-Type': 'application/json',
+            'Cache-Control': 'no-cache',
+        }
+
+        randQuestion = test_data[randrange(len(test_data))]
+
+        response = self.client.get(f'api/answer?question={randQuestion}', headers=headers)
+
+        if response.status_code != 200:
+            print(f"Error: {response.status_code}, Response Text: {response.text}")
